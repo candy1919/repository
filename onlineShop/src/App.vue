c05dc47b9e2673b5e176045bfcc9ca55ced4d5cd
candy1919 <994256595@qq.com>
2017-04-13 09:52:17 +0800

    new floder

---
+++ onlineShop/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+  </div>
+</template>
+
+<script>
+import vHeader from './components/head/vHeader'
+export default {
+  name: 'app',
+  components:{
+    vHeader
+  }
+}
+</script>
+
+<style>
+#app {
+ /* font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;*/
+}
+</style>
